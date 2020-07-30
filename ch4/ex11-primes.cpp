1a35eb93f37dec82b64ad29a8376002da8d24c80
Juan Diaz <juansdiazo@gmail.com>
2020-07-30 18:36:07 +0200

    added more chapter 4 exercises

---
+++ ch4/ex11-primes.cpp
@@ -0,0 +1,22 @@
+#include "../std_lib_facilities.h"
+
+bool isPrime(int test_number, vector<int> primes); // tests if a number is prime
+
+int main(){
+    vector<int> primes{2};      // Considering 2 as the first prime
+    constexpr int first{3};     // first number to test
+    constexpr int last{100};    // last number to test
+
+    for(int test_number=first; test_number<=last; test_number++)        // outer loop, from first to last  
+        if(isPrime(test_number, primes)) primes.push_back(test_number); // inner loop, add if prime
+
+    for(int prime:primes)       // print prime number list on screen
+        cout << prime << " ";   
+    cout << '\n';
+}
+
+bool isPrime(int test_number, vector<int> primes){
+    for(int prime:primes)
+        if (test_number%prime == 0) return false; // residual 0 when divided by number primes vector      
+    return true;                                  // note: in this example all primes are < than test number
+}
