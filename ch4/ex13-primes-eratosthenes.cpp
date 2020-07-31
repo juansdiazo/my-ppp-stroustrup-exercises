f3f1b21a7f2e35be06f304483f7e0272d09fe267
Juan Diaz <juansdiazo@gmail.com>
2020-07-31 17:08:11 +0200

    added more chapter 4 exercises

---
+++ ch4/ex13-primes-eratosthenes.cpp
@@ -0,0 +1,46 @@
+
+// Finde primes using "Sieve of Erastothenes" method
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    constexpr int max{100};             // last number in check list              
+    int current{0}, prime{2}, idx{0};   // Initialize variables
+    bool stop = false;                  // to stop outer loop
+    vector<char> check_list(max);       // list with check values (vector of bools not possible)
+    
+    fill (check_list.begin(),check_list.end(),'u'); // u: unmarked, p:prime, c:composite(not prime)    
+    check_list[0] = 'c';   // 1 is not prime
+    check_list[1] = 'p';   // 2 is prime
+
+    // outer loop: mark all numbers in range with composite or prime 
+    while (!stop){
+
+        // inner loop 1: mark prime factors of current prime as composite (not prime)
+        current = prime;
+        for (int step=2; current<=max; step++){
+            current = prime*step;  
+            check_list[current-1] = 'c';  // marked as not prime
+        } 
+
+        // inner loop 2: search for next prime
+        for (idx=prime; idx<max; idx++){ // search next prime
+            if(idx==(max-1) ) { // when last element to mark
+                if (check_list[idx]=='u') check_list[idx] = 'p'; // check if it is prime
+                stop = true; // finish outer loop
+            } else if (check_list[idx]=='u'){ // if unmarked means it is prime
+                check_list[idx] = 'p'; // mark as prime
+                prime = idx+1; // use for next iteration of outer loop
+                break;
+            }
+        }
+    }
+    
+    for(int i=0; i<check_list.size(); i++){       // print prime number list on screen
+        if(check_list[i] == 'p') cout  << i+ 1 << " ";
+    }   
+    cout << '\n';
+    
+}
+
