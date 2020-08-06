8da4e882354ad610e49502f71127e15d8ba92d07
Juan Diaz <juansdiazo@gmail.com>
2020-08-06 10:59:17 +0200

    Added exercise 4 chapter 5

---
+++ ch5/ex4-ctok-check-function.cpp
@@ -0,0 +1,17 @@
+#include "../std_lib_facilities.h"
+
+double ctok(double c)       // converts Celsius to Kelvin
+{
+    // pre-condition: value must be higher than absolute zero (-273.15)
+    if (c<-273.15) error("Impossible temperature (too low)"); 
+    int k = c + 273.15;
+    return k;
+}
+
+int main()
+{
+    double c = 0;           // declare input variable
+    cin >> c;               // retrieve temperature to input variable
+    double k = ctok(c);     // convert temperature
+    cout << k << '\n';      // print out temperature
+}
