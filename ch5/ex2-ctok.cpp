02c9fb4de177d73d0d43b189f8ef0358a082db18
Juan Diaz <juansdiazo@gmail.com>
2020-08-06 10:39:48 +0200

    Added exercise 2 chapter 5

---
+++ ch5/ex2-ctok.cpp
@@ -0,0 +1,19 @@
+#include "../std_lib_facilities.h"
+
+double ctok(double c)       // converts Celsius to Kelvin
+{
+    int k = c + 273.15;
+    // return int           // error 1: invalid type of return (syntax)
+    return k;
+}
+
+int main()
+{
+    double c = 0;           // declare input variable
+    // cin >> d;            // error 2 typo (logic)
+    cin >> c;               // retrieve temperature to input variable
+    // double k = ctok("c");// error 3: type error
+    double k = ctok(c);     // convert temperature
+    // Cout <<k<<'/n';      // error 4: invalid function and symbol / (syntax)
+    cout << k << '\n';      // print out temperature
+}
