4e82770b116568a67558a50eb46576111abce515
Juan Diaz <juansdiazo@gmail.com>
2020-09-12 21:07:58 +0200

    added exercise 3 chapter 8

---
+++ ch8/ex3-fibo-xy.cpp
@@ -0,0 +1,26 @@
+#include "../std_lib_facilities.h"
+
+void print(const string &label, const vector<int> &myvector);
+
+void fibonacci(int x, int y, vector<int> &v, int n);
+
+int main(){
+    vector<int> myvector;
+    fibonacci(1,1, myvector, 10);
+    print("Fibonacci-like series", myvector);
+}
+
+// prints a prompt string and the contents of a vector
+void print(const string &label, const vector<int> &myvector){
+    cout << label << " : ";
+    for(int el:myvector) cout << el << " ";
+    cout << '\n';
+}
+
+// generates a fibonacci-like sequence of n numbers
+// given the first two elements of the series x and y
+void fibonacci(int x, int y, vector<int> &v, int n){
+    v.push_back(x);
+    v.push_back(y);    
+    for (int i=2; i<n; i++) v.push_back(v[i-2] + v[i-1]);
+}
