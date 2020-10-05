4c8f83355ab7364feec170a04da01fdb11896855
Juan Diaz <juansdiazo@gmail.com>
2020-10-05 18:53:39 +0200

    add drill part 1 chapter 17

---
+++ ch17/drill1-array-vectors.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print_array10(ostream& os, int* a);
+void print_array(ostream& os, int* a,int n);
+
+int main(){
+    // 1. Allocate an array of 10 ints on the free store using new
+    //int *p = new int[10]{1,2,3,4,5,6,7,8,9,10};
+    constexpr int s = 10;
+    int *p = new int[s];
+
+    // 2. Print the values of the 10 ints to cout
+    for (int i=0;i<s;i++) cout << p[i] <<" ";
+    cout << endl;
+
+    // 3. Deallocate the array using delete[]
+    delete[] p;
+
+    // 4. write function print_array10 
+
+    // 5. Allocate an array of 10 ints on the free store; initialize it with the values
+    //    100,101,102,etc and print out its values
+    int *q = new int[s]{100,101,102,103,104,105,106,107,108,109};
+    print_array10(cout,q); 
+    delete[] q;
+
+    // 6. Allocate an array of 11 ints on the free store; initialize it and print its values
+    int *r = new int[11]{1,2,3,4,5,6,7,8,9,10,11};
+    for (int i=0;i<11;i++) cout << r[i] << " ";
+    cout << endl;
+    delete[] r;
+
+    // 7. write function print_array
+
+    // 8. Allocate an array of 20 ints on the free store; initialize it with the values
+    //    100,101,102,etc and print out its values 
+    int *t = new int[20]{100,101,102,103,104,105,106,107,108,109,
+                         110,111,112,113,114,115,116,117,118,119,};
+    print_array(cout,t,20);
+    delete[] t;
+
+    // 9. Delete arrays (all deleted after printing)
+    return 0;
+}
+
+// 4. Write a function that prints out the values of a (assumed to have 10 elements) to os
+void print_array10(ostream& os, int* a){
+    for (int i=0;i<10;i++) os << a[i] << " ";
+    os << endl;
+}
+
+// 7. Write a function that prints out the values of a (assumed to have n elements) to os
+void print_array(ostream& os, int* a,int n){
+    for (int i=0;i<n;i++) os << a[i] << " ";
+    os << endl;
+}
