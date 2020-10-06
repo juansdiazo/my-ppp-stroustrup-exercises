65e69fd8d5694d48bce4beab7ccfd312f2cdee0b
Juan Diaz <juansdiazo@gmail.com>
2020-10-06 21:04:12 +0200

    add drill part 1 chapter 17, vector implementation

--- ch17/drill1-array-vectors.cpp
+++ ch17/drill1-array-vectors.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void print_array10(ostream& os, int* a);
 void print_array(ostream& os, int* a,int n);
+void print_vector(ostream& os, vector<int> &vec);
 
 int main(){
     // 1. Allocate an array of 10 ints on the free store using new
@@ -37,11 +38,23 @@ int main(){
     // 8. Allocate an array of 20 ints on the free store; initialize it with the values
     //    100,101,102,etc and print out its values 
     int *t = new int[20]{100,101,102,103,104,105,106,107,108,109,
-                         110,111,112,113,114,115,116,117,118,119,};
+                         110,111,112,113,114,115,116,117,118,119};
     print_array(cout,t,20);
     delete[] t;
 
     // 9. Delete arrays (all deleted after printing)
+
+    // 10. Do exercises 5,6, and 8 using a vector instead of an array 
+    //     and a print_vector instead of print_array
+    
+    vector<int> vec10{100,101,102,103,104,105,106,107,108,109};     // Ex.5
+    vector<int> vec11{1,2,3,4,5,6,7,8,9,10,11};                     // Ex.6
+    vector<int> vec20{100,101,102,103,104,105,106,107,108,109,      // Ex.8
+                      110,111,112,113,114,115,116,117,118,119};
+    print_vector(cout,vec10);
+    print_vector(cout,vec11);
+    print_vector(cout,vec20);
+
     return 0;
 }
 
@@ -54,5 +67,11 @@ void print_array10(ostream& os, int* a){
 // 7. Write a function that prints out the values of a (assumed to have n elements) to os
 void print_array(ostream& os, int* a,int n){
     for (int i=0;i<n;i++) os << a[i] << " ";
+    os << endl;
+}
+
+// 10. Write print_vector instead of print_array
+void print_vector(ostream& os, vector<int> &vec){ 
+    for (int el:vec) os << el << " ";
     os << endl;
 }
